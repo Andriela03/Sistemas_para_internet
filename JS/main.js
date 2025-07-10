5c352fcb49180789a672be3388f7fa402f56e0a2
Andriela Gleice Pereira da Silva <20231041110026@ifrn.local>
2025-07-10 17:57:31 -0300

    Aula JavaScript (10/07): Aprendendo funções, variáveis e lista

---
+++ JS/main.js
@@ -0,0 +1,3 @@
+/* let varivel1
+var variavel3 = Variáveis_que_podem_ser_modificadas
+const variavel2  Variáveis que não podem ser modificadas */
--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,84 @@ let pessoa = {
 }
 
 console.log (pessoa.nome)
+
+
+////
+
+
+let idade = 18;
+
+if (idade < 18) {
+    console.log('Você pode ter acesso.');
+}
+else if (idade >= 18 && idade < 25) {
+    console.log('Você pode entrar com ressalvas...');
+}
+else {
+    console.log("Você pode ter acesso");
+}
+
+
+
+////
+
+
+let array = [10, "Andriela", true, "Gleice"]
+
+const objetos = {nome: "Andriela", idade:29, sexo:"F" }
+
+console.log(objetos['nome'])
+
+
+const usuarios = [{
+    nome:"Andriela",
+    idade: 19,
+    sexo: "F", 
+    endereco: {
+        rua: "rua",
+        cidade: "Cidade",
+        numero: "Número"
+    }
+}, {
+    nome:"Gleice",
+    idade: 20,
+    sexo: "F", 
+    endereco: {
+        rua: "rua lelele",
+        cidade: "lalala",
+        numero: "9"
+    }
+}
+]
+
+console.log(usuarios[0].endereco.rua)
+console.log(usuarios[1].nome)
+
+//Se o usuario não tiver coocado nome, 
+// o nome padrão dele será user111
+//se ele colocar o nome, apresenta o nome 
+
+const nomeUsuario = ""
+const nomePadrao = nomeUsuario || 'user111'
+
+console.log(nomePadrao)
+
+
+function soma(valor1, valor2) {
+    return valor1 + valor2
+}
+
+console.log(soma(10, 15))
+
+
+function somaTodosValores(valores) {
+    let soma = 0
+    for(let i=0; i<valores.length; i++) {
+        soma = soma + valores[1]
+        if (typeof(valores[i]) !== 'number') 
+        {return 'Valor inválido'}
+    }
+    return soma
+}
+
+console.log(somaTodosValores(10, 29, 30, 40))
