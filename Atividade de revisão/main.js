450de29e92ad3f5b02279387e77dcf6edb424b87
Andriela Gleice Pereira da Silva <20231041110026@ifrn.local>
2025-07-18 13:55:36 -0300

    Fazendo atividade de revisão

---
+++ Atividade de revisão/main.js
@@ -0,0 +1,26 @@
+//Questão 1
+console.log('Bem-vindo!')
+
+
+//Questão 2
+let preco = [10.00, 25.00, 15.00, 19.99]
+let produtos = ['Leite', 'Café', 'Bolo', 'Nutella']
+let clientes = ['Tiago', 'Barreto', 'Andriela', 'Gleice']
+
+//Questão 3 
+let produto = 25.00
+let desconto = 10/100
+
+let valorDesconto = produto * (desconto)
+let precoProdutoDescontado = produto - valorDesconto
+
+
+//Preço descontado
+console.log(`Preço descontado: ${valorDesconto}`)
+//Valor do produto
+console.log(`Valor do produto: ${produto}`)
+//Soma valor total com desconto
+console.log(`Valor com desconto ${precoProdutoDescontado}`)
+
+
+//Questão 4
